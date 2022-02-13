2ec714b2de1e9d6f3a6438dde4fc6b080af4accc
as6823917 <66206385+as6823917@users.noreply.github.com>
2022-02-13 14:00:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,9 +15,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
-GROUPS = []
-for grp in environ.get("GROUPS").split():
-    GROUPS.append(int(grp))
+
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
